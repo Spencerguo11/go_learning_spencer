7c0a821364f4a0031832e3a9df5fd0001a8c5934
Spencerguo11 <spencer.guo11@gmail.com>
2024-05-14 15:04:14 -0600

    Add doc.go

---
+++ doc.go
@@ -0,0 +1,5 @@
+// package go_learning_spencer a basic, example repository for creating
+// go_learning_spencer Go packages, hosted on Github, complete with basic CI
+// configuration.
+
+package go_learning_spencer
